76006bad129c11b51afc738fec822ed87f27e5df
SlavaSavul <slavik_savul@mail.ru>
2018-09-19 21:07:32 +0300

    3

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ function my() {
 
 console.log(my());*/
 
-////////////5.1//////////////////////////
+////////////5.2//////////////////////////
 var fs = require("fs");
+
 var testFolder = process.argv[2];
 testFolder=testFolder.replace('\\','\\\\');
 testFolder=testFolder.split('\\').join('\\\\');
@@ -33,4 +34,38 @@ walker.on('end', function() {\
 ")
 
 
-//////////////////////////////////////
+/////////////////5.3/////////////////////
+var copyright;
+var mkdirp = require('mkdirp');
+var walk    = require('walk');
+var path = require('path');
+
+
+fs.readFile("config.json", "utf8", function(error,data){ 
+    copyright=JSON.parse(data).copyright;
+
+    console.log(copyright);
+
+
+	mkdirp('testFolder', function(err) { 
+
+
+var files   = [];
+var walker  = walk.walk(testFolder, { followLinks: false });
+walker.on('file', function(root, stat, next) {
+	console.log(stat);
+    next();
+});
+walker.on('end', function() {
+    console.log(files);
+});
+	});
+
+});
+
+
+
+
+
+
+
