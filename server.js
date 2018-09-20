80acc3409397a3249b697e9794ba63b735ec6796
SlavaSavul <slavik_savul@mail.ru>
2018-09-20 12:12:25 +0300

    4

--- server.js
+++ server.js
@@ -19,8 +19,13 @@ var walk    = require('walk');
 var path = require('path');
 var fs = require("fs");
 var new_dir;
-
+var copyright;
+var dir_path=process.argv[2];
 var testFolder = process.argv[2];
+
+
+
+
 fs.writeFileSync("summary.js", "\
 var walk    = require('walk');\
 var files   = [];\
@@ -39,12 +44,21 @@ walker.on('end', function() {\
 
 doTask();
 
+
+
 function doTask(){
 	  new_dir = `${process.argv[2]}/${path.basename(process.argv[2])}`;
-	  console.log(new_dir);
+	  //console.log(new_dir);
+	  fs.readFile("config.json", "utf8", readCopyright);
+	  copy(createFolder());
 }
 
-function create_folder() {
+function readCopyright(error,data){ 
+    copyright=JSON.parse(data).copyright;
+}
+
+
+function createFolder() {
     fs.exists(new_dir, function (exists) {
         if (!exists) {
             fs.mkdir(new_dir, (err) => {
@@ -56,7 +70,7 @@ function create_folder() {
     });
 }
 
-function copy_files(generate_dir){
+function copy(){
     fs.readdir(dir_path, function (err, items) {
         if(err){
             console.log(err);
@@ -64,31 +78,28 @@ function copy_files(generate_dir){
             for(let i = 0; i<items.length; i++){
                 let file_name = items[i].toString();
                 if(/.*\.txt/.test(file_name)){
-                    create_file(file_name);
+                    createNewFile(file_name);
                 }
             }
         }
     })
 }
 
-function create_file(file_path){
+function createNewFile(file_path){
     fs.readFile(dir_path+'/'+file_path, 'utf8', (err, data)=> {
         if(err){
             console.log(err);
         }else{
-            add_text(file_path, copyright+data+copyright);
+            fs.appendFile(new_dir+'/'+file_path, copyright+data+copyright, 'utf8', (err)=> {
+                if(err) {
+                  console.log(err);
+                 }
+            }); 
         }
     });
 }
 
-function add_text(file_path, text){
-    fs.appendFile(new_dir+'/'+file_path, text, 'utf8', (err)=> {
-        if(err) {
-            console.log(err);
-        }
-    });
-}
-
+//////////////////////////////////////////
 function watchChanges() {
     fs.watch(new_dir, (eventType, filename) => {
         console.log(`event type is: ${eventType}`);
