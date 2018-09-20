5f1e4f300c2f67afe8b80f062e71d7fece6a9e33
SlavaSavul <slavik_savul@mail.ru>
2018-09-20 11:55:10 +0300

    4

--- server.js
+++ server.js
@@ -14,11 +14,13 @@ function my() {
 console.log(my());*/
 
 ////////////5.2//////////////////////////
+var mkdirp = require('mkdirp');
+var walk    = require('walk');
+var path = require('path');
 var fs = require("fs");
+var new_dir;
 
 var testFolder = process.argv[2];
-testFolder=testFolder.replace('\\','\\\\');
-testFolder=testFolder.split('\\').join('\\\\');
 fs.writeFileSync("summary.js", "\
 var walk    = require('walk');\
 var files   = [];\
@@ -34,37 +36,69 @@ walker.on('end', function() {\
 ")
 
 
-/////////////////5.3/////////////////////
-var copyright;
-var mkdirp = require('mkdirp');
-var walk    = require('walk');
-var path = require('path');
 
+doTask();
 
-fs.readFile("config.json", "utf8", function(error,data){ 
-    copyright=JSON.parse(data).copyright;
-	mkdirp('testFolder', function(err) { 
+function doTask(){
+	  new_dir = `${process.argv[2]}/${path.basename(process.argv[2])}`;
+	  console.log(new_dir);
+}
 
+function create_folder() {
+    fs.exists(new_dir, function (exists) {
+        if (!exists) {
+            fs.mkdir(new_dir, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+    });
+}
 
-var files   = [];
-var walker  = walk.walk(testFolder, { followLinks: false });
-walker.on('file', function(root, stat, next) {
-	
-	if(stat.name.indexOf(".txt")>0){
-	
-		fs.createReadStream(root+ '\\' + stat.name).pipe(fs.createWriteStream('testFolder'+'\\'+stat.name));
-	}   
-	next();
-});
-walker.on('end', function() {
-    
-});
-});
-});
+function copy_files(generate_dir){
+    fs.readdir(dir_path, function (err, items) {
+        if(err){
+            console.log(err);
+        }else{
+            for(let i = 0; i<items.length; i++){
+                let file_name = items[i].toString();
+                if(/.*\.txt/.test(file_name)){
+                    create_file(file_name);
+                }
+            }
+        }
+    })
+}
 
+function create_file(file_path){
+    fs.readFile(dir_path+'/'+file_path, 'utf8', (err, data)=> {
+        if(err){
+            console.log(err);
+        }else{
+            add_text(file_path, copyright+data+copyright);
+        }
+    });
+}
 
+function add_text(file_path, text){
+    fs.appendFile(new_dir+'/'+file_path, text, 'utf8', (err)=> {
+        if(err) {
+            console.log(err);
+        }
+    });
+}
 
-
+function watchChanges() {
+    fs.watch(new_dir, (eventType, filename) => {
+        console.log(`event type is: ${eventType}`);
+        if (filename) {
+            console.log(`filename provided: ${filename.toString()}`);
+        } else {
+            console.log('filename not provided');
+        }
+    });
+}
 
 
 
